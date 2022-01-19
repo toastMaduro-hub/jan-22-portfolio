a04396be3fc4bb2ddf22862e125d70650cd94c2b
Jared Veltsos <jaredveltsos@Jareds-MacBook-Pro.local>
2022-01-18 20:46:27 -1000

    typo about.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,7 +10,7 @@ class About extends React.Component {
                 <h1>About</h1>
                 <p>
                     I don't like to define myself by the work I've done. I define myself by the work that I <span className="purps">want</span> to do. Skills can be taught, Personality is inherent. 
-                    I like to <span className="purps">learn</span>, challenging myself and do interesting things that have a purpose. 
+                    I like to <span className="purps">learn</span>, challenge myself and do interesting things that have a purpose. 
                     <br />
                     <br />
                     My Abundent energy fuels me in the pursuit of many interests, hobbies and areas of study. I'm a fast learner, able to pick up new skills and juggle different projects and roles with <span className="purps">enthusiasm</span>.

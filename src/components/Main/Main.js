ee2b8f5c2345473ee53734296bff911e7c74e3ed
Jared Veltsos <jaredveltsos@Jareds-MacBook-Pro.local>
2022-01-22 22:10:56 -1000

    fixing down button

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,43 +5,6 @@ import MainIntro from "./MainIntro";
 import './Main.css'
 import { RiArrowDownSLine, RiArrowUpSLine } from "react-icons/ri";
 
-
-
-
-// class Main extends React.Component {
-
-//     render() {
-//         return (
-//             <section>
-//                 <div className="main-container">
-//                     <div className="inner-container">
-//                         <h1>
-//                             Hey There!&nbsp;<span className="wave">👋🏾</span>
-//                         </h1>
-//                         <br />
-//                         <h1>
-//                             I'M <strong> JARED VELTSOS</strong>
-//                         </h1>
-//                         <br />
-//                         <Type />
-//                     </div>
-//                     <div className="down-arrow">
-//                         <a href="#myTarget"><RiArrowDownSLine /></a>
-//                     </div>
-//                 </div>
-//                 <div id='myTarget'>
-//                     <Skills />
-//                 </div>
-//                 <div>
-//                     <MainIntro />
-//                 </div>
-//             </section>
-//         )
-//     }
-// }
-
-// export default Main;
-
 function Main() {
 
     // const skillRef = useRef();
@@ -100,3 +63,40 @@ function Main() {
 export default Main;
 
 
+
+
+
+
+// class Main extends React.Component {
+
+//     render() {
+//         return (
+//             <section>
+//                 <div className="main-container">
+//                     <div className="inner-container">
+//                         <h1>
+//                             Hey There!&nbsp;<span className="wave">👋🏾</span>
+//                         </h1>
+//                         <br />
+//                         <h1>
+//                             I'M <strong> JARED VELTSOS</strong>
+//                         </h1>
+//                         <br />
+//                         <Type />
+//                     </div>
+//                     <div className="down-arrow">
+//                         <a href="#myTarget"><RiArrowDownSLine /></a>
+//                     </div>
+//                 </div>
+//                 <div id='myTarget'>
+//                     <Skills />
+//                 </div>
+//                 <div>
+//                     <MainIntro />
+//                 </div>
+//             </section>
+//         )
+//     }
+// }
+
+// export default Main;

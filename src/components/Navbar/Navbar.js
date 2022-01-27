8cf42cdfe2fa090eb663c76280d7b9fd6e58c7d5
Jared Veltsos <jaredveltsos@Jareds-MacBook-Pro.local>
2022-01-26 15:15:36 -1000

    nav-bar logo <Link>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavItems from "./NavItems"
 import Button from "../../Assets/Button/Button"
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 class Navbar extends React.Component {
 
@@ -9,24 +10,24 @@ class Navbar extends React.Component {
         super(props);
         this.state = { clicked: false };
         this.handleClick = this.handleClick.bind(this);
-      }
-     
-      handleClick() {
+    }
+
+    handleClick() {
         this.setState({
-          clicked: !this.state.clicked
+            clicked: !this.state.clicked
         });
-      }
+    }
 
     render() {
         return (
             <div className='sticky'>
                 <nav className="NavbarItems">
-                    <h1 className="navbar-logo">JV</h1>
+                    <Link to='/' style={{ textDecoration: 'none' }} ><h1 className="navbar-logo">JV</h1></Link>
                     <div className="menu-icon" onClick={this.handleClick}>
                         <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                     </div>
                     <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                        
+
                         {/* Returning each icon with map */}
 
                         {NavItems.map((item, index) => {
